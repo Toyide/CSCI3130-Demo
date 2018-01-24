e7b18f9191b028f0bf3a702cbde52cb6a0c97c44
Yide <yd907592@dal.ca>
2018-01-24 04:16:06 -0400

    The user should input text on the field and click the button after. The text from the field should then be copied to the textbox that was already in the layout

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.acme.csci3130_demo;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    Button btn;
+    TextInputEditText tf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=(TextView) findViewById(R.id.TV);
+        btn=(Button) findViewById(R.id.btn);
+        tf=(TextInputEditText) findViewById(R.id.TF);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View V){
+                tv.setText(tf.getText());
+            }
+        });
     }
 }
